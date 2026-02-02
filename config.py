612570dcd028dfa512260037130ab88f8007c29a
spoilmisscandyrotten-crypto <spoilmisscandyrotten@icloud.com>
2026-02-02 02:49:58 -0800

    Create config.py
    Add config

---
+++ config.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = "PASTE_TELEGRAM_BOT_TOKEN_HERE"
+
+# You will set this after you run /myid once.
+ADMIN_CHAT_ID = 0
+
+TIMEZONE = "America/Los_Angeles"
+
+# Deposit policy
+DEPOSIT_PER_HOUR = 100
+INCALL_DEPOSIT_THRESHOLD_HOURS = 6
